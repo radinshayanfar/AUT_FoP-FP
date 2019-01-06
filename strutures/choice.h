86dc87dff160b44a43de4684822a188a4039c4a2
Radin Shayanfar <radin.shayanfar@gmail.com>
2019-01-07 00:27:13 +0330

    choice structure defined

---
+++ strutures/choice.h
@@ -0,0 +1,16 @@
+/******************
+* 0 1 0 1 0 0 1 1 *
+* 0 1 0 0 0 0 0 1 *
+* 0 1 0 0 1 0 0 0 *
+* 0 1 0 0 0 0 0 1 *
+* 0 1 0 1 0 0 1 0 *
+******************/
+
+// (c) 2019 - Radin Shayanfar
+
+#include "parameters.h"
+
+struct Choice {
+    char choice[200];
+    struct Parameters effect;
+};
