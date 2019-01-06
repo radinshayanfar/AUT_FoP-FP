81456894b7c9030f9fc5f622b0ade22c5ae06c84
Radin Shayanfar <radin.shayanfar@gmail.com>
2019-01-07 00:54:16 +0330

    headers and sources for structures

---
+++ strutures/headers/user.h
@@ -0,0 +1,16 @@
+/******************
+* 0 1 0 1 0 0 1 1 *
+* 0 1 0 0 0 0 0 1 *
+* 0 1 0 0 1 0 0 0 *
+* 0 1 0 0 0 0 0 1 *
+* 0 1 0 1 0 0 1 0 *
+******************/
+
+// (c) 2019 - Radin Shayanfar
+
+#include "parameters.h"
+
+struct User {
+    char name[100];
+    struct Parameters user_params;
+};
