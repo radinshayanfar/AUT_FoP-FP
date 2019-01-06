f0afbf499dfa953582f9fc567ea802af871ea3d5
Radin Shayanfar <radin.shayanfar@gmail.com>
2019-01-07 00:30:43 +0330

    problem structure defined

---
+++ strutures/problem.h
@@ -0,0 +1,16 @@
+/******************
+* 0 1 0 1 0 0 1 1 *
+* 0 1 0 0 0 0 0 1 *
+* 0 1 0 0 1 0 0 0 *
+* 0 1 0 0 0 0 0 1 *
+* 0 1 0 1 0 0 1 0 *
+******************/
+
+// (c) 2019 - Radin Shayanfar
+
+#include "choice.h"
+
+struct Problem {
+    char problem[200];
+    struct Choice choice1, choice2;
+};
